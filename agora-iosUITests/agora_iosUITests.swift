c2081a0fa511bd20ec9ba021db9ab672007bbef8
Siddharth <40488679+siddharth952@users.noreply.github.com>
2020-08-24 11:27:52 +0530

    Add test launch performance.

--- agora-iosUITests/agora_iosUITests.swift
+++ agora-iosUITests/agora_iosUITests.swift
@@ -168,12 +168,12 @@ class agora_iosUITests: XCTestCase {
         
     }
 
-//    func test_launch_performance() {
-//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-//            // This measures how long it takes to launch your application.
-//            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-//                XCUIApplication().launch()
-//            }
-//        }
-//    }
+    func test_launch_performance() {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
 }
